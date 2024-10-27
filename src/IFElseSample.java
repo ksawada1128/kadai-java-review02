87436a382b772fea7d5b44aab0ff9e5d2414305d
ksawada1128 <kenta.sawada.1618@gmail.com>
2024-10-27 21:15:33 +0900

    Lesson4サンプルコード

---
+++ src/IFElseSample.java
@@ -0,0 +1,13 @@
+
+public class IFElseSample {
+
+    public static void main(String[] args) {
+        int score = -50;
+        if (score >= 60) {
+            System.out.println("合格です！");
+        }else {
+            System.out.println("赤点です...");
+        }
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,17 @@
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 5;
+        if(a == b && b == c) {
+            System.out.println("正三角形");
+        }else if(a != b && b != c) {
+            System.out.println("不等辺三角形");
+        }else {
+            System.out.println("二等辺三角形");
+        }
+    }
+
+}
